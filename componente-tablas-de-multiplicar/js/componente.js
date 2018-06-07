cf40ea33b927276a3a2e4f124bd6282315800e62
sergarb1 <sergi.profesor@gmail.com>
2018-06-07 16:28:09 +0200

    Comentarios incluidos

--- componente-tablas-de-multiplicar/js/componente.js
+++ componente-tablas-de-multiplicar/js/componente.js
@@ -1,3 +1,5 @@
+// Componente de Vue2JS https://vuejs.org/v2/guide/components.html
+
 Vue.component('componente-tablas', {
 	template: `
 		<div class="c-tablas">
@@ -9,11 +11,14 @@ Vue.component('componente-tablas', {
 					<form>
 						<div class="form-group">
 							<label for="num" class="control-label">Introduzca un n&uacute;mero:</label>
+							<!-- Usamos v-model para enlazar el input a datos https://vuejs.org/v2/guide/forms.html -->
 							<input type="number" id="num" class="form-control" v-model="numero" autofocus>
 						</div>
 					</form>
+					<!-- v-show, similar a usar el Display de CSS (se renderiza pero se ve o no se ve) https://vuejs.org/v2/guide/conditional.html#v-show -->
 					<table class="table table-bordered table-hover table-condensed" v-show="numero">
 						<tbody>
+							<!-- v-for con un rango https://vuejs.org/v2/guide/list.html#v-for-with-a-Range -->
 							<tr v-for="n in 10" class="text-center">
 								<td>{{ numero }}</td>
 								<td>x</td>
