15328260fe9b834a30af9bb6d9b00bf4b5872f85
sergarb1 <sergi.profesor@gmail.com>
2018-06-07 16:33:36 +0200

    Mas comentarios:1

--- consumir-api-con-vuejs/js/app.js
+++ consumir-api-con-vuejs/js/app.js
@@ -3,6 +3,11 @@ new Vue({
 	data: {
 		usuarios: []
 	},
+	/*
+	Al ser creada la aplicacion de VUE2JS https://vuejs.org/v2/guide/instance.html#Instance-Lifecycle-Hooks
+	Hacemos una peticion GET a la URL. Al recibir la respuesta en formato JSON, simplemente
+	mapeamos dicha respuesta a la variable usuarios
+	*/
 	created: function(){
 		this.$http.get('https://jsonplaceholder.typicode.com/users').then(function(response){
 			this.usuarios = response.data;
